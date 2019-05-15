7dfc89a6b04a9e64f4c6cc9395cd8abd05404837
Razem <info@razem.cz>
2019-05-15 15:59:08 +0200

    Fix no result in run script

--- run.py
+++ run.py
@@ -14,15 +14,18 @@ def main(argv, argc):
   contractId = int(argv[0])
   loadSuppliers = argv[1] == 'true' if argc > 1 else False
 
-  res = predictContractWinner(cur, contractId, loadSuppliers)
-  print('candidates:', res['candidates'])
-  print('prediction:', res['prediction'])
+  res = None#predictContractWinner(cur, contractId, loadSuppliers)
+  if res:
+    print('candidates:', res['candidates'])
+    print('prediction:', res['prediction'])
 
-  if loadSuppliers:
-    print('suppliers:', res['suppliers'])
-    success = calculateSuccess(res)
-    for key, value in success.items():
-      print(key, value)
+    if loadSuppliers:
+      print('suppliers:', res['suppliers'])
+      success = calculateSuccess(res)
+      for key, value in success.items():
+        print(key, value)
+  else:
+    print('No result!')
 
 if __name__ == '__main__':
   main(sys.argv[1:], len(sys.argv) - 1)
